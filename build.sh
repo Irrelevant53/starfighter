d4dcc5dfbc6e5f491e567ce9791c35b22f4086ce
sareni <46085131+Irrelevant53@users.noreply.github.com>
2021-04-30 09:40:54 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export LD_LIBRARY_PATH=/home/mx/Desktop/Starfighter/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/mx/Desktop/starfighter/lib:$LD_LIBRARY_PATH
 
-gcc -o starfighter -Wall -Wextra /home/mx/Desktop/Starfighter/src/animations.c /home/mx/Desktop/Starfighter/src/enemy.c /home/mx/Desktop/Starfighter/src/particles.c /home/mx/Desktop/Starfighter/src/bullet.c /home/mx/Desktop/Starfighter/src/files.c /home/mx/Desktop/Starfighter/src/player.c /home/mx/Desktop/Starfighter/src/ui.c /home/mx/Desktop/Starfighter/src/game.c /home/mx/Desktop/Starfighter/src/camera.c  -L/home/mx/Desktop/Starfighter/lib -lraylib -lpthread -lm -ldl -I/home/mx/Desktop/Starfighter/include
+gcc -o starfighter -Wall -Wextra /home/mx/Desktop/starfighter/src/animations.c /home/mx/Desktop/starfighter/src/enemy.c /home/mx/Desktop/starfighter/src/particles.c /home/mx/Desktop/starfighter/src/bullet.c /home/mx/Desktop/starfighter/src/files.c /home/mx/Desktop/starfighter/src/player.c /home/mx/Desktop/starfighter/src/ui.c /home/mx/Desktop/starfighter/src/game.c /home/mx/Desktop/starfighter/src/camera.c  -L/home/mx/Desktop/starfighter/lib -lraylib -lpthread -lm -ldl -I/home/mx/Desktop/starfighter/include
 ./starfighter
