8b4a999a86dc2ede27a3a9e3120431437b5a22c4
sareni <46085131+drito256@users.noreply.github.com>
2024-05-19 22:16:01 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-export LD_LIBRARY_PATH=/home/mx/Desktop/starfighter/lib:$LD_LIBRARY_PATH
-
-gcc -o starfighter -Wall -Wextra /home/mx/Desktop/starfighter/src/animations.c /home/mx/Desktop/starfighter/src/enemy.c /home/mx/Desktop/starfighter/src/particles.c /home/mx/Desktop/starfighter/src/bullet.c /home/mx/Desktop/starfighter/src/files.c /home/mx/Desktop/starfighter/src/player.c /home/mx/Desktop/starfighter/src/ui.c /home/mx/Desktop/starfighter/src/game.c /home/mx/Desktop/starfighter/src/camera.c  -L/home/mx/Desktop/starfighter/lib -lraylib -lpthread -lm -ldl -I/home/mx/Desktop/starfighter/include
-./starfighter
